2650887bd17d08a072f07d2cd9090e2c66fa5ba8
mjhehn <mjhehn@gmail.com>
2018-01-07 17:45:03 -0700

    fixed typo with '

--- Projects/LocalHost.php
+++ Projects/LocalHost.php
@@ -8,7 +8,7 @@ include('../common/navBox.php');
     <div class="content">
         <div class="grid-row-1">
                 <div class="grid-item-1">
-                    <br><br><br><span style="text-align: center"><h2>'ASL VR Experience - RAMhack 2017 1st Place</h2></span><br>
+                    <br><br><br><span style="text-align: center"><h2>ASL VR Experience - RAMhack 2017 1st Place</h2></span><br>
                     <p style="text-align: left">
                         Team Localhost's submission for Colorado State University's 2017 Virtual Reality "RAMhack" hackathon. As per the guidelines of the hackathon and its contest, 'Fat Tire' was built by a four-person team over the course of 48 hours and was a concept promoting 'Scholarship, research, and community connection' in keeping with Colorado State University's Land-grant mission.
                         <br><br>Over the course of the weekend, Team Localhost designed and developed a prototype learning tool which could recognize a set of American Sign Language hand signs and properly translate them into text. This text was displayed onscreen in a virtual environment alongside references to show users the correct signs they were trying to emulate. As a final demo of the intended concept, users were challenged to sign a word ("Bed"), and if they were able to successfully sign all three letters, a 3D model of a bed was spawned into the space to reinforce the meaning of the word.
